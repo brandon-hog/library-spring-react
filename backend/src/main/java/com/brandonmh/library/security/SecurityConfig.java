8f93c031aa99ed128eddec2312cb00a137c8a606
brandon-UWF <bmh86@students.uwf.edu>
2026-03-05 21:52:10 -0600

    moving security config to security folder

--- backend/src/main/java/com/brandonmh/library/security/SecurityConfig.java
+++ backend/src/main/java/com/brandonmh/library/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.brandonmh.library.config;
+package com.brandonmh.library.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
